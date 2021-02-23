79fd32418155be1363b895ceb20054295748e043
vnguyen <vinh.nguyen@bia-boeing.com>
2021-02-22 20:12:53 -0500

    add python readme

--- clue-less-server/api.py
+++ clue-less-server/api.py
@@ -6,6 +6,6 @@ app.config["DEBUG"] = True
 
 @app.route('/', methods=['GET'])
 def home():
-    return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
+    return "<h1>HasBros Homepage of server</p>"
 
 app.run()
