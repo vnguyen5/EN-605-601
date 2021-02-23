d3ac4513c8b0d617042ffa50f8acbcb9cd717086
vnguyen <vinh.nguyen@bia-boeing.com>
2021-02-22 19:18:56 -0500

    Scaffolding

---
+++ clue-less-server/src/test/java/HasBros/cluelessserver/ClueLessServerApplicationTests.java
@@ -0,0 +1,13 @@
+package HasBros.cluelessserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClueLessServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
